b3368d14e7357505776d71bc520b98eadca877f6
Daniel Ufeli <danielufeli@yahoo.com>
2022-02-15 16:48:25 +0100

    setup init

---
+++ src/components/TodoItem.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TodoItem = (props) => {
+  return <li>{props.todo.title}</li>
+}
+
+export default TodoItem
---
+++ src/components/TodosList.js
@@ -0,0 +1,14 @@
+import React from "react"
+import TodoItem from "./TodoItem";
+class TodosList extends React.Component {
+    render(){
+        return(
+            <ul>
+            {this.props.todos.map(todo => (
+              <TodoItem key={todo.id} todo={todo} />
+            ))}
+          </ul> 
+        )
+    }
+}
+export default TodosList
